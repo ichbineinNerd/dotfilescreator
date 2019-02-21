d3fdb2731a42c36fc25dee4570217bfa9a5df298
Tilman B <groessternerdallerzeiten@web.de>
2019-02-21 21:00:10 +0100

    f*** consistency

--- src/com/nerdyyy/dotfilesgen/Main.java
+++ src/com/nerdyyy/dotfilesgen/Main.java
@@ -2,25 +2,58 @@ package com.nerdyyy.dotfilesgen;
 
 import javafx.application.Application;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private ObjectProperty<IDotFileGenerator> currentGenerator = null;
+    private ObjectProperty<IDotFileGenerator> currentGenerator = new SimpleObjectProperty<>();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("DotfilesGenerator");
-        ScrollPane root = new ScrollPane();
+        GridPane root = new GridPane();
+        ScrollPane sp = new ScrollPane();
 
-        currentGenerator.addListener((observable, oldValue, newValue) -> root.setContent(newValue.GenerateLayout()));
+        root.setVgap(5);
+        root.setHgap(5);
+
+        //sp.setFitToHeight(true);
+        sp.setPrefViewportWidth(Integer.MAX_VALUE); //looks weird with Double.MAX_VALUE, and 2.1 billion should still be large enough for most purposes.
+        sp.setMaxWidth(Double.MAX_VALUE);
+        sp.setFitToWidth(true);
+        sp.setFitToHeight(true);
+        sp.setStyle("-fx-box-border: transparent;");
+
+
+        sp.prefWidthProperty().bind(root.widthProperty().divide(3).multiply(2));
+        root.add(sp, 0, 0, 1, 1);
+        FlowPane fp = new FlowPane();
+        fp.prefWidthProperty().bind(root.widthProperty().divide(3));
+        root.add(fp, 1, 0, 2, 1);
+
+
+        Scene sc = new Scene(root, 640, 480);
+        primaryStage.setScene(sc);
+
+        currentGenerator.addListener(
+                (observable, oldValue, newValue) ->{
+                    if (newValue == null)
+                        return;
+                    Node tmp = newValue.GenerateLayout();
+                    ((VBox)tmp).prefWidthProperty().bind(sc.widthProperty());
+                    sp.setContent(tmp);
+                });
         currentGenerator.setValue(new gitignore());
 
-        root.setFitToHeight(true);
-
-        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
 
--- src/com/nerdyyy/dotfilesgen/gitignore.java
+++ src/com/nerdyyy/dotfilesgen/gitignore.java
@@ -3,27 +3,44 @@ package com.nerdyyy.dotfilesgen;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 class gitignore implements IDotFileGenerator {
+    private Node createSpacer() {
+        final Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        return spacer;
+    }
     private void AddRow(VBox vb) {
         HBox hbox = new HBox();
+        hbox.setMaxWidth(Double.MAX_VALUE);
         hbox.setPadding(new Insets(0, 5, 0, 5));
         hbox.setSpacing(2);
 
         TextField txt = new TextField("file name");
+        txt.setMinWidth(100d);
 
         Button delete = new Button("X");
         delete.setStyle("-fx-border-color: red");
         delete.setOnAction(event -> {if (textFields.size() > 1) {textFields.remove(txt); vb.getChildren().remove(hbox);}});
         textFields.add(txt);
 
-        hbox.getChildren().addAll(txt, new CheckBox("case-sensitive"), delete);
+        CheckBox cb = new CheckBox("case-sensitive");
+        cb.setMinWidth(75d);
+
+        delete.setMinWidth(50d);
+        hbox.getChildren().addAll(txt, createSpacer(), cb, createSpacer(), delete);
+        HBox.setHgrow(txt, Priority.ALWAYS);
+        HBox.setHgrow(cb, Priority.ALWAYS);
+        HBox.setHgrow(delete, Priority.ALWAYS);
+
 
         vb.getChildren().add(vb.getChildren().size() - 1, hbox);
     }
@@ -34,7 +51,9 @@ class gitignore implements IDotFileGenerator {
         VBox vbox = new VBox();
         vbox.setPadding(new Insets(10));
         vbox.setSpacing(10);
-
+        vbox.setMaxWidth(Double.MAX_VALUE);
+        vbox.setPrefWidth(Integer.MAX_VALUE);
+        vbox.setFillWidth(true);
         HBox hbox = new HBox();
         Button newRow = new Button("+");
         newRow.setStyle("-fx-color: green;");
@@ -43,9 +62,9 @@ class gitignore implements IDotFileGenerator {
         newRow.setOnAction(event -> AddRow(vbox));
         hbox.getChildren().add(newRow);
         vbox.getChildren().add(hbox);
-
         AddRow(vbox);
 
+        vbox.setStyle("-fx-box-border: transparent;");
         return vbox;
     }
     public String getGeneratedFileContents() {
