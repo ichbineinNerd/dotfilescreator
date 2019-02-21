2ee6149fb663537efb54b9a1a0604d9f31c55750
Tilman B <groessternerdallerzeiten@web.de>
2019-02-21 19:09:30 +0100

    update root pane on generator change

--- src/com/nerdyyy/dotfilesgen/Main.java
+++ src/com/nerdyyy/dotfilesgen/Main.java
@@ -1,19 +1,23 @@
 package com.nerdyyy.dotfilesgen;
 
 import javafx.application.Application;
+import javafx.beans.property.ObjectProperty;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private ObjectProperty<IDotFileGenerator> currentGenerator = null;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("DotfilesGenerator");
         ScrollPane root = new ScrollPane();
 
+        currentGenerator.addListener((observable, oldValue, newValue) -> root.setContent(newValue.GenerateLayout()));
+        currentGenerator.setValue(new gitignore());
 
-        root.setContent(new gitignore().GenerateLayout());
         root.setFitToHeight(true);
 
         primaryStage.setScene(new Scene(root, 400, 400));
